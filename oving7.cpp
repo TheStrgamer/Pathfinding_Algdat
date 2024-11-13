442df19f2338e6e8e197fb46de780b7b0f1fa760
Konrad <konradseime@gmail.com>
2024-11-13 15:16:22 +0100

    formating

--- oving7.cpp
+++ oving7.cpp
@@ -24,20 +24,20 @@ int main() {
 
     //precomputeDriveTimes(nodes, { 435,8777,12356 }, "driveTimes.txt");
 
+    cout << "From node:\tTo node:\tCalculate time\tNodes visited\tNodes in path:\tDrive time:" << endl;
     for (int i = 1; i <= 13; i++) {
         const vector<int> toAndFrom = getTestCase(i);
         const int fromNode = toAndFrom[0];
         const int toNode = toAndFrom[1];
+        cout << fromNode << "\t\t" << toNode << "\t\t";
 
         const Result result = dijkstraShortestPath(nodes, fromNode, toNode);
         if (result.time == -1) {
             cout << "No path found" << endl;
         } else {
-            cout << "Path has " << result.path.size() << " nodes" << endl;
-            cout << endl;
-            cout << "Time: " << result.hours << " hours, " << result.minutes << " minutes, " << result.seconds << " seconds" << endl;
+            cout << result.path.size() << "\t\t";
+            cout  << result.hours << ":" << result.minutes << ":" << result.seconds << endl;
         }
-        cout << endl;
     }
 
     return 0;
